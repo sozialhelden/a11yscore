d30aac5ab6446605361d6a5c23755182e1d8def3
Kris Siepert <m@kris.cool>
2025-07-24 16:40:27 +0200

    chore: disable health-check for initial deployment

--- server/routes/health.get.ts
+++ server/routes/health.get.ts
@@ -3,21 +3,22 @@ import { osm_amenities } from "~/db/schema/osm-sync";
 import { results } from "~/db/schema/results";
 
 export default defineEventHandler(async (_event) => {
-	try {
-		await useStorage().set("health", "ok");
-		await resultsDb.select({ id: results.id }).from(results).limit(1);
-		await osmSyncDb
-			.select({ id: osm_amenities.id })
-			.from(osm_amenities)
-			.limit(1);
-	} catch (_error) {
-		console.error("Healthcheck failed:", _error);
-		throw createError({
-			status: 500,
-			statusMessage: "Server error",
-			message: "Service is not healthy",
-		});
-	}
+	// TODO: enable again after initial deployment
+	// try {
+	// 	await useStorage().set("health", "ok");
+	// 	await resultsDb.select({ id: results.id }).from(results).limit(1);
+	// 	await osmSyncDb
+	// 		.select({ id: osm_amenities.id })
+	// 		.from(osm_amenities)
+	// 		.limit(1);
+	// } catch (_error) {
+	// 	console.error("Healthcheck failed:", _error);
+	// 	throw createError({
+	// 		status: 500,
+	// 		statusMessage: "Server error",
+	// 		message: "Service is not healthy",
+	// 	});
+	// }
 
 	return { healthy: true };
 });
