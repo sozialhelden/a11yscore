bee9c73551cd6f554fcce1cc7d3814bc86a96873
Kris Siepert <m@kris.cool>
2025-11-03 14:29:35 +0100

    feat: add bullmq

--- src/a11yscore/routes/v1/get-score.ts
+++ src/a11yscore/routes/v1/get-score.ts
@@ -1,3 +1,4 @@
+import { useIsDevelopment } from "~/utils/env";
 import { allowedAdminAreas } from "~~/src/a11yscore/config/admin-areas";
 import {
   type SubCategoryId,
@@ -9,116 +10,123 @@ import { type CriterionId, criteria } from "~~/src/a11yscore/config/criteria";
 import { type TopicId, topics } from "~~/src/a11yscore/config/topics";
 import { queryScoreResultsByAdminArea } from "~~/src/a11yscore/queries/query-score-results-by-admin-area";
 
-export default defineEventHandler(async (event) => {
-  const adminArea = getRouterParam(event, "adminArea");
-  const adminAreaId = allowedAdminAreas.find(
-    ({ slug }) => slug === adminArea,
-  )?.id;
+export default defineCachedEventHandler(
+  async (event) => {
+    const adminArea = getRouterParam(event, "adminArea");
+    const adminAreaId = allowedAdminAreas.find(
+      ({ slug }) => slug === adminArea,
+    )?.id;
 
-  if (!adminAreaId) {
-    throw createError({
-      status: 404,
-      statusMessage: "Not found",
-      message: "Admin area not found",
-    });
-  }
+    if (!adminAreaId) {
+      throw createError({
+        status: 404,
+        statusMessage: "Not found",
+        message: "Admin area not found",
+      });
+    }
 
-  const {
-    scoreResults,
-    topLevelCategoryScoreResults,
-    subCategoryScoreResults,
-    topicScoreResults,
-    criterionScoreResults,
-  } = await queryScoreResultsByAdminArea(adminAreaId);
+    const {
+      scoreResults,
+      topLevelCategoryScoreResults,
+      subCategoryScoreResults,
+      topicScoreResults,
+      criterionScoreResults,
+    } = await queryScoreResultsByAdminArea(adminAreaId);
 
-  if (!scoreResults) {
-    throw createError({
-      status: 404,
-      statusMessage: "Not found",
-      message: "Calculated score for admin area not found",
-    });
-  }
+    if (!scoreResults) {
+      throw createError({
+        status: 404,
+        statusMessage: "Not found",
+        message: "Calculated score for admin area not found",
+      });
+    }
 
-  // TODO: refactor this
-  const result = topLevelCategoryScoreResults.map(
-    ({ id, topLevelCategory, score }) => {
-      const topLevelCategoryProperties =
-        topLevelCategories[topLevelCategory as TopLevelCategoryId];
-      return {
-        id: topLevelCategory,
-        name: topLevelCategoryProperties.name(),
-        score,
-        interpretation: topLevelCategoryProperties.interpretation?.(score),
-        subCategories: subCategoryScoreResults
-          .filter(
-            ({ topLevelCategoryScoreId }) => topLevelCategoryScoreId === id,
-          )
-          .map(({ id, subCategory, score }) => {
-            const subCategoryProperties =
-              subCategories[subCategory as SubCategoryId];
-            return {
-              id: subCategory,
-              name: subCategoryProperties.name(),
-              score,
-              description: subCategoryProperties.description?.(),
-              osmTags: subCategoryProperties.osmTags,
-              topics: topicScoreResults
-                .filter(({ subCategoryScoreId }) => subCategoryScoreId === id)
-                .map(({ id, topic, score }) => ({
-                  id: topic,
-                  name: topics[topic as TopicId].name(),
-                  score,
-                  criteria: criterionScoreResults
-                    .filter(({ topicScoreId }) => topicScoreId === id)
-                    .map(({ criterion, score }) => {
-                      const criterionPivotProperties =
-                        subCategoryProperties.topics
-                          .find(({ topicId }) => topic === topicId)
-                          .criteria.find(
-                            ({ criterionId }) => criterionId === criterion,
-                          );
+    // TODO: refactor this
+    const result = topLevelCategoryScoreResults.map(
+      ({ id, topLevelCategory, score }) => {
+        const topLevelCategoryProperties =
+          topLevelCategories[topLevelCategory as TopLevelCategoryId];
+        return {
+          id: topLevelCategory,
+          name: topLevelCategoryProperties.name(),
+          score,
+          interpretation: topLevelCategoryProperties.interpretation?.(score),
+          subCategories: subCategoryScoreResults
+            .filter(
+              ({ topLevelCategoryScoreId }) => topLevelCategoryScoreId === id,
+            )
+            .map(({ id, subCategory, score }) => {
+              const subCategoryProperties =
+                subCategories[subCategory as SubCategoryId];
+              return {
+                id: subCategory,
+                name: subCategoryProperties.name(),
+                score,
+                description: subCategoryProperties.description?.(),
+                osmTags: subCategoryProperties.osmTags,
+                topics: topicScoreResults
+                  .filter(({ subCategoryScoreId }) => subCategoryScoreId === id)
+                  .map(({ id, topic, score }) => ({
+                    id: topic,
+                    name: topics[topic as TopicId].name(),
+                    score,
+                    criteria: criterionScoreResults
+                      .filter(({ topicScoreId }) => topicScoreId === id)
+                      .map(({ criterion, score }) => {
+                        const criterionPivotProperties =
+                          subCategoryProperties.topics
+                            .find(({ topicId }) => topic === topicId)
+                            .criteria.find(
+                              ({ criterionId }) => criterionId === criterion,
+                            );
 
-                      const criterionProperties =
-                        criteria[criterion as CriterionId];
+                        const criterionProperties =
+                          criteria[criterion as CriterionId];
 
-                      const genericRecommendations =
-                        criterionProperties.recommendations();
+                        const genericRecommendations =
+                          criterionProperties.recommendations();
 
-                      return {
-                        id: criterion,
-                        name: criterionProperties.name(),
-                        score,
-                        reason:
-                          criterionPivotProperties?.reason?.() ||
-                          criterionProperties.reason(),
-                        recommendations:
-                          criterionPivotProperties?.recommendations?.(
-                            genericRecommendations,
-                          ) || genericRecommendations,
-                        links:
-                          criterionPivotProperties?.links?.() ||
-                          criterionProperties.links?.() ||
-                          [],
-                        osmTags: criterionProperties.osmTags,
-                      };
-                    }),
-                })),
-            };
-          }),
-      };
-    },
-  );
-
-  delete scoreResults.adminAreaId;
-
-  return {
-    score: {
-      ...scoreResults,
-      adminArea: {
-        id: adminAreaId,
-        name: allowedAdminAreas.find(({ id }) => id === adminAreaId)?.name,
+                        return {
+                          id: criterion,
+                          name: criterionProperties.name(),
+                          score,
+                          reason:
+                            criterionPivotProperties?.reason?.() ||
+                            criterionProperties.reason(),
+                          recommendations:
+                            criterionPivotProperties?.recommendations?.(
+                              genericRecommendations,
+                            ) || genericRecommendations,
+                          links:
+                            criterionPivotProperties?.links?.() ||
+                            criterionProperties.links?.() ||
+                            [],
+                          osmTags: criterionProperties.osmTags,
+                        };
+                      }),
+                  })),
+              };
+            }),
+        };
       },
-      toplevelCategories: result,
-    },
-  };
-});
+    );
+
+    delete scoreResults.adminAreaId;
+
+    return {
+      score: {
+        ...scoreResults,
+        adminArea: {
+          id: adminAreaId,
+          name: allowedAdminAreas.find(({ id }) => id === adminAreaId)?.name,
+        },
+        toplevelCategories: result,
+      },
+    };
+  },
+  {
+    maxAge: 60 * 60 /* 1 hour */,
+    varies: ["accept-language", "Accept-Language"],
+    shouldBypassCache: useIsDevelopment,
+  },
+);
