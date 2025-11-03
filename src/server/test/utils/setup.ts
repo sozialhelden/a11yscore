56670300b499122c24c014064e455cce5f1fa732
Kris Siepert <m@kris.cool>
2025-11-03 10:07:44 +0100

    fix: hash in base-url

--- src/server/test/utils/setup.ts
+++ src/server/test/utils/setup.ts
@@ -1,3 +1,11 @@
 import "dotenv/config";
 
-export const BASE_URL = process.env.NITRO_TEST_BASE_URL;
+const baseUrl = new URL(
+  process.env.NITRO_TEST_BASE_URL || "http://localhost:3000",
+);
+
+// make sure it works even if a path or hash is provided
+baseUrl.hash = "";
+baseUrl.pathname = "/";
+
+export const BASE_URL = baseUrl.toString();
