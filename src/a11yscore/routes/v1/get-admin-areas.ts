d0e4830b058f250d1b2f823096d642978b799fb3
Kris Siepert <m@kris.cool>
2025-11-05 16:55:23 +0100

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:src/a11yscore/routes/v1/get-admin-areas.ts

--- src/a11yscore/routes/v1/get-admin-areas.ts
+++ src/a11yscore/routes/v1/get-admin-areas.ts
@@ -1,14 +1,29 @@
+import { sql } from "drizzle-orm";
+import { osmSyncDb } from "~/db";
+import { osm_admin, osm_admin_gen0 } from "~/db/schema/osm-sync";
 import { useIsDevelopment } from "~/utils/env";
 import { allowedAdminAreas } from "~~/src/a11yscore/config/admin-areas";
-import {sql} from "drizzle-orm";
-import {osmSyncDb} from "~/db";
-import {osm_admin} from "~/db/schema/osm-sync";
 
-export default defineEventHandler(
-  async () => {
-      const query = sql`SELECT * FROM ${osm_admin} LIMIT 5`;
+export default defineEventHandler(async () => {
+  const germany = -51477;
+  const allowedAdminLevels = [4];
 
-    //return osmSyncDb.execute(query);
-      return "hello world";
-  },
-);
+  const query = sql`
+    SELECT ${osm_admin.osm_id}, ${osm_admin.name}, ${osm_admin.admin_level}, ${osm_admin.wikidata}
+    FROM ${osm_admin}
+    JOIN ${osm_admin_gen0} ON ${osm_admin_gen0.osm_id} = ${germany}
+    WHERE 
+        ST_Covers(
+            ${osm_admin_gen0.geometry},
+            -- Checking only for a representative point inside the sub-region avoids
+            -- missing sub-regions with exclaves and non-exact border topologies (like
+            -- Bavaria in Germany). 
+            ST_PointOnSurface(${osm_admin.geometry})
+        )
+        AND ${osm_admin.admin_level} IN ${allowedAdminLevels}
+        AND ${osm_admin.name} != ''
+    LIMIT 20
+  `;
+
+  return (await osmSyncDb.execute(query)).rows;
+});
