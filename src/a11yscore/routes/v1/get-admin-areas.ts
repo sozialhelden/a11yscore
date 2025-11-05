5755aa0eb6bd5ddeba809bdd52d20489c12f4bd3
Kris Siepert <m@kris.cool>
2025-11-05 17:42:37 +0100

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:src/a11yscore/routes/v1/get-admin-areas.ts

--- src/a11yscore/routes/v1/get-admin-areas.ts
+++ src/a11yscore/routes/v1/get-admin-areas.ts
@@ -1,17 +1,29 @@
 import { sql } from "drizzle-orm";
+import slug from "slug";
 import { osmSyncDb } from "~/db";
 import { osm_admin, osm_admin_gen0 } from "~/db/schema/osm-sync";
 import { useIsDevelopment } from "~/utils/env";
-import {AdminArea, allowedAdminAreas} from "~~/src/a11yscore/config/admin-areas";
-import slug from "slug";
+import {
+  AdminArea,
+  allowedAdminAreas,
+} from "~~/src/a11yscore/config/admin-areas";
 
 export type AdminAreaResult = {
-    osm_id: number;
-    name: string;
-    admin_level: number;
-    wikidata: string;
+  osm_id: number;
+  name: string;
+  admin_level: number;
+  wikidata: string;
 };
-slug.extend({'ü': 'ue', 'ä': 'ae', 'ö': 'oe', 'ß': 'ss'})
+slug.extend({ ü: "ue", ä: "ae", ö: "oe", ß: "ss" });
+
+function generateHash(): string {
+  const chars = "abcdefghjkmnopqrstuvwxyz0123456789"; // excluded 'l' and 'i'
+  let result = "";
+  for (let j = 0; j < 6; j++) {
+    result += chars.charAt(Math.floor(Math.random() * chars.length));
+  }
+  return result;
+}
 
 export default defineEventHandler(async () => {
   const germany = -51477;
@@ -33,8 +45,13 @@ export default defineEventHandler(async () => {
         AND ${osm_admin.name} != ''
     LIMIT 20
   `;
-    const adminAreas = (await osmSyncDb.execute<AdminAreaResult>(query)).rows;
-    return  adminAreas.map((area) => {
-        return {...area, hash: "hash", slug: slug(area.name)};
-    })
+
+  const adminAreas = (await osmSyncDb.execute<AdminAreaResult>(query)).rows;
+
+  // scores/-51235-berlin
+  // scores/berlin-s3hd
+
+  return adminAreas.map((area) => {
+    return { ...area, hash, slug: slug(area.name) };
+  });
 });
