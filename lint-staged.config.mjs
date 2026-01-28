472ab8eb6db36a37e460802b2d9fee9158fef981
Kris Siepert <m@kris.cool>
2026-01-28 15:03:26 +0100

    fix: package-lock.json

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -3,6 +3,6 @@
  * @type {import('lint-staged').Configuration}
  */
 export default {
-  "**/*.{js,ts,json}": ["biome format --write", "biome lint --write"],
+  "**/*.{js,ts}": ["biome format --write", "biome lint --write"],
   "**/*.{md}": ["alex -w"],
 };
