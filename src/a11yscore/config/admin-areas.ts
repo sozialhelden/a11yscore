ef143e6f4ea72f1c49a0a45dc0f476198c836ce2
Lynn <64597238+Mayaryin@users.noreply.github.com>
2026-02-05 11:23:17 +0100

    fix: exclude bremen country (as opposed to bremen city)

--- src/a11yscore/config/admin-areas.ts
+++ src/a11yscore/config/admin-areas.ts
@@ -7,3 +7,9 @@ export const allowedAdminLevels = [
   5, // = regierungsbezirk
   6, // = landkreis/kreisfreie stadt
 ];
+
+export const excludedAdminAreas = [
+  // Country of Bremen, includes Bremerhaven.
+  // We exclude this to avoid duplicate with Bremen City and Bremerhaven City
+  -62718,
+];
