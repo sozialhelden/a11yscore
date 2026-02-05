56f62efcad755312e8d8a54c9e5fc792501d1fdc
Lynn <64597238+Mayaryin@users.noreply.github.com>
2026-02-05 11:23:17 +0100

    fix: exclude bremen country (as opposed to bremen city)

--- src/a11yscore/routes/v1/get-admin-areas.ts
+++ src/a11yscore/routes/v1/get-admin-areas.ts
@@ -1,11 +1,16 @@
+import { notInArray } from "drizzle-orm";
 import { appDb } from "~/db";
 import { adminAreas } from "~/db/schema/app";
 import { useIsDevelopment } from "~/utils/env";
+import { excludedAdminAreas } from "~~/src/a11yscore/config/admin-areas";
 
 export default defineCachedEventHandler(
   async () => {
     return {
-      adminAreas: await appDb.select().from(adminAreas),
+      adminAreas: await appDb
+        .select()
+        .from(adminAreas)
+        .where(notInArray(adminAreas.osmId, excludedAdminAreas)),
     };
   },
   {
