ae5941ebf0979b32cb39424508fe51a1944f6cfa
Lynn <64597238+Mayaryin@users.noreply.github.com>
2026-05-11 13:40:36 +0200

    feat: add italian cities to sync job

--- src/a11yscore/config/admin-areas.ts
+++ src/a11yscore/config/admin-areas.ts
@@ -1,5 +1,6 @@
 export const allowedCountries = [
   -51477, // Germany
+  -365331,
 ];
 
 export const allowedAdminLevels = [
@@ -12,4 +13,13 @@ export const excludedAdminAreas = [
   // Country of Bremen, includes Bremerhaven.
   // We exclude this to avoid duplicate with Bremen City and Bremerhaven City
   -62718,
+];
+
+export const includedItalianCities = [
+  // We temporarily include Rom, Venedig, Florenz, Mailand, Neapel for an event in the Italian consulate
+  -41485, // Rome
+  -44741, // Venice
+  -42602, // Florence
+  -44915, // Milan
+  -40767, // Naples
 ];
